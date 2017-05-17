2f3aa3dbbeb04d7b8e18423973e59d081a17afca
Piotr Czubkowski <piotrcz@o2.pl>
2017-05-17 16:06:51 +0200

    Create Enum Days

--- src/com/company/EnumSex.java
+++ src/com/company/EnumSex.java
@@ -4,5 +4,5 @@ package com.company;
  * Created by Piotr Czubkowski on 2017-05-17.
  */
 public enum EnumSex {
-    WOMEN, MEN, IDK
+    WOMEN(), MEN(), IDK();
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,17 @@ public class Main {
 
     public static void main(String[] args) {
 
+//        inicjalizacja pola
+        EnumSex sex = EnumSex.MEN;
 
+//       przykladowe wywolanie enuma:
+        switch (sex) {
+            case WOMEN:
+                System.out.println("Kobieta");
+            case MEN:
+                System.out.println("Facet");
+
+        }
+    }
 
 }
